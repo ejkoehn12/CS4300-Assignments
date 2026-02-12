711e85a6a2db346a5caef0eebe44de0fddeb3cc1
Ethan Koehn <mailmaster@ejkoehn.org>
2026-02-12 19:19:59 +0000

    Completed Task 1

---
+++ homework1/src/task1.py
@@ -0,0 +1,7 @@
+#Task: Print out "Hello Worl!"
+
+
+
+#Printing out Hello World!
+message = "Hello World!"
+print(message)
---
+++ homework1/tests/test_task1.py
@@ -0,0 +1,15 @@
+import subprocess
+#Task: Write a test function to make sure that task1.py is returning "Hello World!"
+
+#Function to run and catch output of task1.py
+
+TestCase = "Hello World!"
+def runTask1():
+    command = ["python", "../src/task1.py"]
+    result = subprocess.run(command, capture_output=True, text=True, check=True)
+    #Need to use .strip() on result to get rid of hidden \n that might show up
+    captured_stdout = result.stdout.strip()
+    return captured_stdout
+
+def test_result():
+    assert runTask1() == TestCase
