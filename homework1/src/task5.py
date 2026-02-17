93ff3dc4c0d09e01fa2fce5c1712784206dab991
Ethan Koehn <mailmaster@ejkoehn.org>
2026-02-17 19:24:24 +0000

    Completed Task 5

---
+++ homework1/src/task5.py
@@ -0,0 +1,16 @@
+list_of_books = ["Book1","Book2","Book3","Book4","Book5"]
+
+#Function to return the list_of_boots variable
+def return_list_of_books():
+    return list_of_books
+
+
+student1 = {"name": "John", "student_id": 1234}
+student2 = {"name": "Will", "student_id": 5678}
+student3 = {"name": "Logan", "student_id": 9101}
+student_database = [student1, student2, student3]
+
+#Function to return student_database variable
+def return_student_database():
+    return student_database
+
---
+++ homework1/tests/test_task5.py
@@ -0,0 +1,17 @@
+import sys
+import os
+#Task: Run Test cases for task4.py
+
+#Pointing src outside of the test folder over into the src folder
+sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
+from src import task5
+
+
+
+
+#Test function for return_student_database()
+def test_return_student_database():
+    student_database = task5.return_student_database()
+    assert str(student_database[0].get("name")) == "John" and student_database[0].get("student_id") == 1234
+    assert str(student_database[1].get("name")) == "Will" and student_database[1].get("student_id") == 5678
+    assert str(student_database[2].get("name")) == "Logan" and student_database[2].get("student_id") == 9101
