5e80efcae0ac051db3ba3bb38fd44c9938889b25
Ethan Koehn <mailmaster@ejkoehn.org>
2026-02-17 21:03:02 +0000

    Added on to Task 4

--- homework1/src/task4.py
+++ homework1/src/task4.py
@@ -2,10 +2,17 @@
 
 #Function to calculate discount using a base price input and a discount input as a percentage
 def calculate_discount(base_price, base_discount):
-    discount = base_price * base_discount/100
-    print(discount)
-    return base_price - discount
+    if is_number(base_price) and is_number(base_discount):
+        discount = base_price * base_discount/100
+        print(discount)
+        return base_price - discount
+    else:
+        return "Error, a non numeric character was entered"
 
-
-
-
+#Function to help with verifiying that a number has been inputted
+def is_number(value):
+    try:
+        float(value)
+        return True
+    except ValueError:
+        return False
